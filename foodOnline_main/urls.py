2359559f0829e5677187174a05214750b45c61b2
TranNhatMinh23 <Trannhatminh23082001@gmail.com>
2022-12-29 18:06:42 +0700

    section 23

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -30,4 +30,6 @@ urlpatterns = [
     path('cart/', MarketplaceViews.cart, name='cart'),
     # Search
     path('search/', MarketplaceViews.search, name='search'),
+    # Checkout
+    path('checkout/', MarketplaceViews.checkout, name='checkout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
